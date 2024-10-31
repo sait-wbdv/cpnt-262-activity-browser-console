c3278d3a5996588f6a8a4beb4aa7568b7d78cf9c
temielosait <temitope.elodimuor@sait.ca>
2024-10-31 13:06:25 -0600

    add starter code for activity

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bug Hunt with the Browser Console
+
+A starter code for learning how to use the browser console.
---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+document.getElementById("buggy-button").addEventListener("click", function() {
+    consol.log("Button Clicked!");
+});
+</script>
